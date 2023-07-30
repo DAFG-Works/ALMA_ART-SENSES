33f198e10e7748790d72f47a7a58da3d6fecc541
DAFG-Works <140922558+DAFG-Works@users.noreply.github.com>
2023-07-30 16:50:54 +0200

    Add files via upload

---
+++ ALMA'ART SENSES_REAL/main.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+// Show the scroll-to-top button when the user scrolls down
+window.onscroll = function() {
+    var button = document.querySelector(".scroll-to-top");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+};
